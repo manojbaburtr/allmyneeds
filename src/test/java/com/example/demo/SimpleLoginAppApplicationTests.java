dbcf4a15f54b2353775842c21231f51e60fa68f5
manojbaburtr <manojbabu9495@gmail.com>
2025-06-27 11:37:39 +0530

    added testconfig

--- src/test/java/com/example/demo/SimpleLoginAppApplicationTests.java
+++ src/test/java/com/example/demo/SimpleLoginAppApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.example.simplelogin.SimpleLoginAppApplication;
+
+@SpringBootTest(classes = SimpleLoginAppApplication.class)
 class SimpleLoginAppApplicationTests {
 
 	@Test
